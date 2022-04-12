dd91c348f1cda2c7f86664600c276232053ecf68
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-04-12 16:09:35 +0530

    [Rahmath]Add: Flipping A Coin And Getting its Percentage

---
+++ BasicCoreProgram/FlipCoin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class FlipCoin
+    {
+        const double HEAD = 0.5;
+        int hCount = 0;
+        int tCount = 0;
+        public void flip()
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                Random random = new Random();
+                double num = random.NextDouble();
+                if(num < HEAD)
+                {
+                    tCount++;
+                }
+                else
+                {
+                    hCount++;
+                }
+            }
+            double hPercentage = hCount % 100;
+            double tPercentage = tCount % 100;
+            Console.WriteLine("Percentage of the head coin " + hPercentage);
+            Console.WriteLine("Persentage of the tail coin " + tPercentage);
+        }
+
+    }
+}
---
+++ BasicCoreProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace BasicCoreProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Core Programs");
+            bool end = true;
+            Console.WriteLine("1. FlipCoin\n2. End the Program");
+            while (end)
+            {
+                Console.WriteLine("Take an option to execute");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.flip();
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter Proper Option To Execute");
+                        break;
+
+                }
+
+
+            }
+
+        }
+        
+
+        
+    } 
+}
