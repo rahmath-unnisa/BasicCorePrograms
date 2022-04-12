405d193aa2e1880e7b93a00ceb358021beeb25ce
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-04-12 22:36:27 +0530

    [Rahmath]Add: Computing quotient And Reminder

--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -7,7 +7,7 @@ namespace BasicCoreProgram
         {
             Console.WriteLine("Basic Core Programs");
             bool end = true;
-            Console.WriteLine("1. FlipCoin\n2. Leap Year \n3. Power Of Two \n4. Harmonic Number \n5. Prime Factor \n6.End the Program");
+            Console.WriteLine("1. FlipCoin\n2. Leap Year \n3. Power Of Two \n4. Harmonic Number \n5. Prime Factor \n6.Quotient And Reminder \n7. End the Program");
             while (end)
             {
                 Console.WriteLine("Take an option to execute");
@@ -34,6 +34,11 @@ namespace BasicCoreProgram
                         PrimeFactor primeFactor = new PrimeFactor();
                         primeFactor.factor();
                         break;
+                    case 6:
+                        QuotientReminder quotientReminder = new QuotientReminder();
+                        quotientReminder.qrprogram();
+                        break;
+                   
                     default:
                         Console.WriteLine("Enter Proper Option To Execute");
                         break;
---
+++ BasicCoreProgram/QuotientReminder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class QuotientReminder
+    {
+        public void qrprogram()
+        {
+            int x, y, r, q;
+            Console.WriteLine("Enter the first number:-");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:-");
+            y = Convert.ToInt32(Console.ReadLine());
+            q = x / y;
+            r = x % y;
+            Console.WriteLine("Quotien is: " + q);
+            Console.WriteLine("Reminder is: " + r);
+        }
+    }
+}
