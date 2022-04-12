35aee1da005fc28c38365499ce9ab5d0a2c8702e
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-04-12 23:06:53 +0530

    [Rahmath]Add: Checking Number is Even Or Odd

---
+++ BasicCoreProgram/EvenOrOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class EvenOrOdd
+    {
+        public void EvenOdd()
+        {
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("The number is even:" + num);
+
+            }
+            else
+                Console.WriteLine("The number is odd:" +num);
+        }
+    }
+}
